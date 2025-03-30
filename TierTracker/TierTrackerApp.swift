eb0ab181d62dc247d19109d3559e5ca3855dd119
Miled Mahdoui <m.mahdoui@hotmail.de>
2025-03-30 21:30:01 +0200

    Initial Commit

---
+++ TierTracker/TierTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TierTrackerApp.swift
+//  TierTracker
+//
+//  Created by Miled Mahdoui on 30.03.25.
+//
+
+import SwiftUI
+
+@main
+struct TierTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TierTrackerTests/TierTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TierTrackerTests.swift
+//  TierTrackerTests
+//
+//  Created by Miled Mahdoui on 30.03.25.
+//
+
+import Testing
+@testable import TierTracker
+
+struct TierTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
